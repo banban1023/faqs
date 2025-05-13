2cf8e2a14f8360c161e32bca88e4c9085866e93f
板板 <747657490@qq.com>
2025-05-13 17:58:42 +0800

    部分css

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <section>
+      <header>
+        <img src="@/assets/images/icon-star.svg" alt="" aria-hidden="true">
+        <h1>FAQs</h1>
+      </header>
+      <main>
+        <AccordionCard></AccordionCard>
+      </main>
+    </section>
+  </div>
+</template>
+
+<script>
+import AccordionCard from './components/AccordionCard.vue'
+export default {
+  name: 'App',
+  components: {
+    AccordionCard
+  }
+}
+</script>
+
+<style lang="less">
+@import '@/styles/index.less';
+</style>
---
+++ src/components/AccordionCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+    <div class="section-header" @click="toggleBtn">
+    <h2>What is Frontend Mentor, and how will it help me?</h2>
+      <button aria-label="toogle-accordion">
+        <img :src="srcUrl" alt="">
+      </button>
+    </div>
+    <article>
+      Frontend Mentor offers realistic coding challenges to help developers improve their
+      frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for
+      all levels and ideal for portfolio building.
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      openBtn: require('@/assets/images/icon-plus.svg'),
+      closeBtn: require('@/assets/images/icon-minus.svg'),
+      isOpen: true
+    }
+  },
+  computed: {
+    srcUrl () {
+      return this.isOpen ? this.openBtn : this.closeBtn
+    }
+  },
+  methods: {
+    toggleBtn () {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '@/styles/card.less';
+</style>
